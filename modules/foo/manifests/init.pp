2f63e6b24199166742891f7a39159d950de4379a
claudio-navarro-martinez <claudio.oami@gmail.com>
2017-10-11 16:31:47 +0100

    Update init.pp

--- modules/foo/manifests/init.pp
+++ modules/foo/manifests/init.pp
@@ -7,4 +7,12 @@ class foo {
       shell   => '/bin/bash',
       uid     => '1001',
     }
+    user { 'informix':
+      ensure  => 'present',
+      gid     => '100',
+      groups => ['sudo'],
+      home    => '/home/informix',
+      shell   => '/bin/bash',
+      uid     => '1002',
+    }
 }
