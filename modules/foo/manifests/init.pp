ed6fa5c0509d89aa151c8462eec94260acf9a05f
claudio-navarro-martinez <claudio.oami@gmail.com>
2017-10-11 16:43:30 +0100

    segunda tanda de commits

--- modules/foo/manifests/init.pp
+++ modules/foo/manifests/init.pp
@@ -2,7 +2,6 @@ class foo {
    user { 'oracle':
       ensure  => 'present',
       gid     => '100',
-      groups => ['sudo'],
       home    => '/home/oracle',
       shell   => '/bin/bash',
       uid     => '1001',
@@ -10,7 +9,6 @@ class foo {
     user { 'informix':
       ensure  => 'present',
       gid     => '100',
-      groups => ['sudo'],
       home    => '/home/informix',
       shell   => '/bin/bash',
       uid     => '1002',
